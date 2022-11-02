a8fb3565e6cc988e41182535381e6cdf82ae9e86
Maria Volkova <safety-team.ru@yandex.ru>
2022-11-02 09:35:22 +0700

    feat: add algorithm queue

--- src/components/queue-page/queue-page.tsx
+++ src/components/queue-page/queue-page.tsx
@@ -1,10 +1,105 @@
-import React from "react";
+import React, {FormEvent, useState} from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import style from "../stack-page/stack-page.module.css";
+import {Input} from "../ui/input/input";
+import {Button} from "../ui/button/button";
+import {Circle} from "../ui/circle/circle";
+import {ElementStates} from "../../types/element-states";
+import {Queue} from "./utils";
+import {delay} from "../../utils";
+
+const size: number = 7;
 
 export const QueuePage: React.FC = () => {
+  const [inputValue, setInputValue] = useState<string>('');
+  const [currentIndex, setCurrentIndex] = useState<number>(-1);
+  const [queue] = useState(new Queue<string>(size));
+  const [queueArray, setQueueArray] = useState<(string | undefined)[]>(queue.printQueue());
+  const [length, setLength] = useState<number>(queue.getLength());
+  const [head, setHead] = useState<number>(queue.getHead());
+  const [tail, setTail] = useState<number>(queue.getTail());
+
+
+  const onChange = (e:FormEvent<HTMLInputElement>): void => {
+    const string = e.currentTarget.value.trim();
+    setInputValue(string);
+  }
+
+  const enqueue = async (item: string) => {
+      queue.enqueue(item);
+      setInputValue('')
+      setQueueArray([...queue.printQueue()]);
+      setLength(queue.getLength());
+      setTail(queue.getTail());
+      setCurrentIndex(tail % queue.getSize());
+      await delay(500);
+      setCurrentIndex(-1);
+      await delay(500);
+  }
+
+  const dequeue = async () => {
+    if (queue) {
+      queue.dequeue();
+      setQueueArray([...queue.printQueue()]);
+      setCurrentIndex((head & queue.getSize()));
+      await delay(500);
+      setHead(queue.getHead());
+      setLength(queue.getLength());
+      setCurrentIndex(-1);
+      await delay(500);
+    }
+  }
+
+  const clear = () => {
+    queue.clear();
+    setQueueArray(queue.printQueue());
+    setHead(queue.getHead());
+    setTail(queue.getTail());
+    setLength(queue.getLength());
+  }
+
   return (
     <SolutionLayout title="Очередь">
-
+      <form className={style.form} onSubmit={(e) => e.preventDefault()}>
+        <div className={style.input_group}>
+          <Input
+              onChange={onChange}
+              isLimitText={true}
+              maxLength={4}
+              value={inputValue}
+              extraClass="mr-6"
+          />
+          <Button
+              text="Добавить"
+              disabled={!inputValue || tail === size}
+              extraClass="mr-6"
+              onClick={() => enqueue(inputValue)}
+          />
+          <Button
+              text="Удалить"
+              onClick={() => dequeue()}
+              disabled={queue.isEmpty()}
+          />
+        </div>
+        <Button
+            text="Очистить"
+            disabled={head === 0 && tail === 0}
+            onClick={() => clear()}
+        />
+      </form>
+      <ul className={style.list}>
+        {queueArray.map((item, index) => {
+          return  (
+              <Circle
+                  key={index}
+                  letter={item}
+                  index={index }
+                  head={(index === head && !queue.isEmpty())  ? "head" : ""}
+                  tail={(index === tail - 1 && !queue.isEmpty()) ? "tail" : ""}
+                  state={index === currentIndex ? ElementStates.Changing : ElementStates.Default}
+              />)
+        })}
+      </ul>
     </SolutionLayout>
   );
 };
