90bf5dc63875358c668bdda054b945883f0a413b
Maria Volkova <safety-team.ru@yandex.ru>
2022-11-21 12:11:35 +0700

    feat: add unit test button circle components

---
+++ src/components/ui/button/button.test.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import TestRenderer from 'react-test-renderer';
+import { render, screen, fireEvent } from '@testing-library/react';
+
+import { Button } from './button';
+
+describe('Тестирование компонента Button', () => {
+
+	it('Кнопка c текстом рендерится без ошибок', () => {
+		const button = TestRenderer
+			.create(<Button text='Текст' />)
+			.toJSON()
+		expect(button).toMatchSnapshot()
+	})
+
+	it('Кнопка без текста рендерится без ошибок', () => {
+		const button = TestRenderer
+			.create(<Button />)
+			.toJSON()
+		expect(button).toMatchSnapshot()
+	})
+
+	it('Кнопка заблокирована, рендерится без ошибок', () => {
+		const button = TestRenderer
+			.create(<Button disabled={true} />)
+			.toJSON()
+		expect(button).toMatchSnapshot()
+	})
+
+	it('Кнопка c индикацией загрузки рендерится без ошибок', () => {
+		const button = TestRenderer
+			.create(<Button isLoader={true} />)
+			.toJSON()
+		expect(button).toMatchSnapshot()
+	})
+
+	it('Нажатие на кнопку вызывает колбек', () => {
+		window.alert = jest.fn();
+
+		render(<Button text='Вызов колбека' onClick={() => { alert('Успешный вызов колбека') }} />)
+
+		const button = screen.getByText("Вызов колбека");
+		fireEvent.click(button);
+
+		expect(window.alert).toHaveBeenCalledWith('Успешный вызов колбека');
+	});
+})
---
+++ src/components/ui/circle/circle.test.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import TestRenderer from 'react-test-renderer';
+import { render, screen, fireEvent } from '@testing-library/react';
+
+import { Circle } from './circle';
+import { ElementStates } from '../../../types/element-states';
+
+describe('Тестирование компонента Circle', () => {
+
+	it('Circle без буквы рендерится без ошибок', () => {
+		const circle = TestRenderer
+			.create(<Circle />)
+			.toJSON()
+		expect(circle).toMatchSnapshot()
+	})
+
+	it('Circle с буквами рендерится без ошибок', () => {
+		const circle = TestRenderer
+			.create(<Circle letter='1' />)
+			.toJSON()
+		expect(circle).toMatchSnapshot()
+	})
+
+	it('Circle с head рендерится без ошибок', () => {
+		const circle = TestRenderer
+			.create(<Circle head='1' />)
+			.toJSON()
+		expect(circle).toMatchSnapshot()
+	})
+
+	it('Circle с react-элементом в head рендерится без ошибок', () => {
+		const circle = TestRenderer
+			.create(<Circle head={<Circle />} />)
+			.toJSON()
+		expect(circle).toMatchSnapshot()
+	})
+
+	it('Circle с tail рендерится без ошибок', () => {
+		const circle = TestRenderer
+			.create(<Circle tail='1' />)
+			.toJSON()
+		expect(circle).toMatchSnapshot()
+	})
+
+	it('Circle с react-элементом в tail рендерится без ошибок', () => {
+		const circle = TestRenderer
+			.create(<Circle tail={<Circle />} />)
+			.toJSON()
+		expect(circle).toMatchSnapshot()
+	})
+
+	it('Circle с index рендерится без ошибок', () => {
+		const circle = TestRenderer
+			.create(<Circle index='0' />)
+			.toJSON()
+		expect(circle).toMatchSnapshot()
+	})
+
+	it('Circle с пропом isSmall ===  true рендерится без ошибок', () => {
+		const circle = TestRenderer
+			.create(<Circle isSmall={true} />)
+			.toJSON()
+		expect(circle).toMatchSnapshot()
+	})
+
+	it('Circle в состоянии default рендерится без ошибок', () => {
+		const circle = TestRenderer
+			.create(<Circle state={ElementStates.Default} />)
+			.toJSON()
+		expect(circle).toMatchSnapshot()
+	})
+
+	it('Circle в состоянии changing рендерится без ошибок', () => {
+		const circle = TestRenderer
+			.create(<Circle state={ElementStates.Changing} />)
+			.toJSON()
+		expect(circle).toMatchSnapshot()
+	})
+
+	it('Circle в состоянии modified рендерится без ошибок', () => {
+		const circle = TestRenderer
+			.create(<Circle state={ElementStates.Modified} />)
+			.toJSON()
+		expect(circle).toMatchSnapshot()
+	})
+
+})
