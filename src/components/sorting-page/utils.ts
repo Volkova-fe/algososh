268fde3fa4fbe28f34595a9262bd31be6b9811ec
Maria Volkova <safety-team.ru@yandex.ru>
2022-11-23 10:21:18 +0700

    feat: add unit test for sorting-page

--- src/components/sorting-page/utils.ts
+++ src/components/sorting-page/utils.ts
@@ -16,3 +16,35 @@ export const swap = (arr: IRandomArr[], firstIndex: number, secondIndex: number)
 	arr[firstIndex] = arr[secondIndex];
 	arr[secondIndex] = temp;
 };
+
+export const selectionSort = (array: IRandomArr[]) => {
+	const { length } = array;
+	for (let i = 0; i < length; i++) {
+		let maxInd = i;
+		for (let j = i + 1; j < length; j++) {
+			if (array[maxInd].num < array[j].num) {
+				maxInd = j;
+			}
+		}
+		if (maxInd != i) {
+			swap(array, i, maxInd);
+		}
+	}
+	return array;
+};
+
+export const bubbleSort = (array: IRandomArr[]) => {
+	const { length } = array;
+	for (let i = 0; i < length; i++) {
+		for (let compareIndex = 0; compareIndex < length - i - 1; compareIndex++) {
+			const left = array[compareIndex].num;
+			const right = array[compareIndex + 1].num;
+			if (left < right) {
+				array[compareIndex].num = right;
+				array[compareIndex + 1].num = left;
+			}
+		}
+	}
+
+	return array;
+}
