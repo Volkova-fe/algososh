f4d3631322299b643d036d2abc60cb1ed1bb86e4
Maria Volkova <safety-team.ru@yandex.ru>
2022-11-25 20:03:22 +0700

    feat: add e2e for fibonacci

--- cypress/e2e/fibonacci.spec.cy.js
+++ cypress/e2e/fibonacci.spec.cy.js
@@ -1,4 +1,5 @@
-import { dataCyForm, dataCyInput, dataCySubmit } from "../constants/test";
+import { DELAY_IN_MS } from "../../src/constants/delays";
+import { dataCyForm, dataCyInput, dataCySubmit, mainCircle } from "../constants/test";
 
 
 describe('Корректная работа Фибоначчи', () => {
@@ -16,9 +17,46 @@ describe('Корректная работа Фибоначчи', () => {
 		cy.clock()
 		cy.get(dataCyForm)
 			.within(() => {
-				cy.get(dataCyInput).type('19')
+				cy.get(dataCyInput).type('5')
 				cy.get(dataCySubmit).click()
 				cy.get(dataCyInput).should('be.disabled')
 			})
+
+		cy.tick(DELAY_IN_MS)
+
+		cy.get(mainCircle).children().should('have.length', '1').should('have.text', '1')
+
+		cy.tick(DELAY_IN_MS)	
+		cy.wait(DELAY_IN_MS)
+		
+		cy.get(mainCircle).children().should('have.length', '2').should('have.text', '11')
+
+		cy.tick(DELAY_IN_MS)	
+		cy.wait(DELAY_IN_MS)
+		
+		cy.get(mainCircle).children().should('have.length', '3').should('have.text', '112')
+
+		cy.tick(DELAY_IN_MS)	
+		cy.wait(DELAY_IN_MS)
+		
+		cy.get(mainCircle).children().should('have.length', '4').should('have.text', '1123')
+
+		cy.tick(DELAY_IN_MS)	
+		cy.wait(DELAY_IN_MS)
+		
+		cy.get(mainCircle).children().should('have.length', '5').should('have.text', '11235')
+
+		cy.tick(DELAY_IN_MS)	
+		cy.wait(DELAY_IN_MS)
+
+		cy.get(mainCircle).children().should('have.length', '6').should('have.text', '112358')
+
+		cy.tick(DELAY_IN_MS)	
+		cy.wait(DELAY_IN_MS)
+
+		cy.get(dataCyForm).within(() => {
+			cy.get(dataCyInput).should('have.value', '')
+			cy.get(dataCySubmit).should('be.disabled')
+		})
 	})
 }); 
